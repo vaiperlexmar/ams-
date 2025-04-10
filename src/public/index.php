40923a2c53235c06c2d28a25c69aa878e4b86757
vaiperlexmar <vaiperlexmar@gmail.com>
2025-04-10 19:56:52 +0300

    feat: Add register and login logic

--- src/public/index.php
+++ src/public/index.php
@@ -10,21 +10,38 @@ use DB\DB;
 $db = new DB("default_app-db");
 $userManager = new UserManager();
 
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    var_dump($_POST);
+    switch ($_POST["action"]) {
+        case "register":
+            $password = $userManager->cryptPassword($_POST["password"]);
+            $user = (new User())
+                ->setUsername($_POST["username"])
+                ->setPassword($password)
+                ->setRoles(["ROLE_USER"]);
 
-    $password = $userManager->cryptPassword($_POST["password"]);
-    $user = (new User())
-        ->setUsername($_POST["username"])
-        ->setPassword($password)
-        ->setRoles(["ROLE_USER"]);
+            $userManager->createUserToken($user);
 
-    $userManager->createUserToken($user);
+            $userId = $db->insert("INSERT INTO users (username, password) VALUES (?, ?)", [$user->getUsername(), $user->getPassword()]);
+            $db->insert("INSERT INTO roles (name, owner_id) VALUES (?, ?)", [$user->getRoles()[0], $userId]);
 
-    $lastId = $db->insert("INSERT INTO users (username, password) VALUES (?, ?)", [$user->getUsername(), $user->getPassword()]);
+            break;
+        case "login":
+            $userFromDataBase = $db->select("SELECT * FROM users WHERE username = ?", [$_POST["username"]]);
+            $roles = $db->select("SELECT * FROM roles WHERE owner_id = ?", [$userFromDataBase[0]["id"]]);
 
-    var_dump($userManager->getUserToken());
-    var_dump($lastId);
+            $user = (new User())
+                ->setUsername($userFromDataBase[0]["username"])
+                ->setPassword($userFromDataBase[0]["password"])
+                ->setRoles($roles)
+                ->setEnabled((bool)$userFromDataBase[0]["enabled"]);
+
+            $userManager = new UserManager();
+            if ($userManager->isPasswordValid($user, $_POST["password"])) {
+                $userManager->createUserToken($user);
+            } else {
+                throw new Exception("Invalid username or password");
+            }
+            break;
+    }
 }
 
